ecb8159be72fc1bb0a9d57eec0cb9428a6724197
ninjavaz <d1n22@onet.pl>
2021-06-01 22:27:06 +0200

    Some changes

--- src/flask-website.py
+++ src/flask-website.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def home():
     return "Hello <h1>WORLD</h1>"
+
+@app.route("/<name>")
+def name(name):
+    return f"Name: {name}"
+
 
 
 
