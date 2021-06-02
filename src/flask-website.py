4ff9bf330f68d04c2b47d7ac91c7075a89e13b32
ninjavaz <d1n22@onet.pl>
2021-06-02 10:50:25 +0200

    logout added

--- src/flask-website.py
+++ src/flask-website.py
@@ -24,5 +24,11 @@ def user(usr):
     else:
         return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
