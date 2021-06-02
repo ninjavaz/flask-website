5195f84ba2fd8faf2867bd65cf800fbf17dce88d
ninjavaz <d1n22@onet.pl>
2021-06-02 09:59:52 +0200

    login page added

--- src/flask-website.py
+++ src/flask-website.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content="Testing")
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
