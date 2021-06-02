7c04d075e966be388ffae9261b9226b2f17eb51d
ninjavaz <d1n22@onet.pl>
2021-06-02 09:11:45 +0200

    redirecting remove

--- src/flask-website.py
+++ src/flask-website.py
@@ -6,16 +6,5 @@ app = Flask(__name__)
 def home():
     return "Hello <h1>WORLD</h1>"
 
-@app.route("/<name>")
-def name(name):
-    return f"Name: {name}"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
-
-
-
-
 if __name__ == "__main__":
     app.run()
